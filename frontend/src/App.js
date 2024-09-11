9380324f35abb5dc5dd9d08e4081109e1482ca62
imprince-kushwaha <mijadon593@hidelux.com>
2024-09-11 22:51:48 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+
+import { Outlet } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <div className="">
+      <Outlet/>
+    </div>
+  );
+}
+
+export default App;
