ae62f430e26fe3d1bbbaa708d387168b2e4a4acf
imprince-kushwaha <mijadon593@hidelux.com>
2024-09-18 12:37:17 +0530

    added react-redux and toaster

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 
 import { Outlet } from 'react-router-dom';
 import './App.css';
+import toast, { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
+    <>
+    <Toaster/>
     <div className="">
       <Outlet/>
     </div>
+    </>
   );
 }
 
