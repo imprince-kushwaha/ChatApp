2e304d6d13e6f0ad93ff809bb5f5b7223acac12e
imprince-kushwaha <mijadon593@hidelux.com>
2024-09-19 12:46:39 +0530

    redux

---
+++ frontend/src/redux/userSlice.js
@@ -0,0 +1,40 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+    _id: "",
+    name: "",
+    email: "",
+    // picture: "",
+    profile_pic: "",
+    token: ""
+}
+
+export const userSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        setUser:(state,action)=>{
+            state._id=action.payload._id
+            state.name=action.payload.name
+            state.email=action.payload.email
+            state.profile_pic=action.payload.profile_pic
+            // state.picture=action.payload.picture
+        },
+        setToken:(state,action)=>{
+            state.token=action.payload
+        },
+        logout:(state,action)=>{
+            state._id=""
+            state.name=""
+            state.email=""
+            // state.picture=""
+            state.profile_pic=""
+            state.token=""
+        }
+    },
+})
+
+// Action creators are generated for each case reducer function
+export const { setUser,setToken,logout} = userSlice.actions
+
+export default userSlice.reducer
