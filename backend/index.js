86436a32f847246e6b05e6b333bc7dd39c0d8e0f
imprince-kushwaha <mijadon593@hidelux.com>
2024-09-25 23:53:49 +0530

    cors fixed

--- backend/index.js
+++ backend/index.js
@@ -35,7 +35,10 @@ dotenv.config();
 //     credentials: true, // If you want to allow credentials (cookies, authorization headers, etc.)
 // };
 // const app = express()
-app.use(cors())
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
